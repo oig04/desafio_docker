0650def977d69574ac4dfb4295436c6a6f36439d
Giovanna Freitas <santos.giovanna04@gmail.com>
2025-06-01 18:56:02 -0300

    Atualiza arquivos do exercício 13 e adiciona workflow CI

--- docker-exercicio-13/app.py
+++ docker-exercicio-13/app.py
@@ -1,8 +1,4 @@
 from datetime import datetime
-import pytz
 
-fuso_horario = pytz.timezone('America/Sao_Paulo')
-data_e_horario_atuais = datetime.now(fuso_horario)
-
-print("\nData e Horário atual:")
-print(data_e_horario_atuais.strftime("%d/%m/%Y %H:%M:%S"))
+now = datetime.now()
+print("Data e hora atual:", now.strftime("%d/%m/%Y %H:%M:%S"))
